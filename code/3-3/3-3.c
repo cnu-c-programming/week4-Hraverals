afc7065903257a00133520afaed209e78694d622
Hraverals <hableflap1379@gmail.com>
2026-03-27 10:38:13 +0900

    오답 수정

--- code/3-3/3-3.c
+++ code/3-3/3-3.c
@@ -2,7 +2,7 @@
 
 int main() {
     printf("sizeof(int*) = %zu\n", sizeof(int*));
-    printf("sizeof(double) = %zu\n", sizeof(double*));
+    printf("sizeof(double*) = %zu\n", sizeof(double*));
     printf("sizeof(char*) = %zu\n", sizeof(char*));
     printf("sizeof(void*) = %zu\n", sizeof(void*));
 }
